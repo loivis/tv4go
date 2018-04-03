24279020ed7d004e1ec6cbd93470c120a9a61e32
Infrastruktur-mobben <infrastruktur@tv4.se>
2018-04-03 13:44:06 +0200

    remove search from stopstream: allow user to stop a stream regardless of valid or not

--- handlers/stopstream.go
+++ handlers/stopstream.go
@@ -1,14 +1,9 @@
 package handlers
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"os"
-
-	search "github.com/TV4/search-go"
 )
 
 func Stopstream(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +16,6 @@ func Stopstream(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
-	site := queryIn.Get("site")
 	videoID := queryIn.Get("video_id")
 	userID := queryIn.Get("user_id")
 	response.VideoID = videoID
@@ -33,32 +27,12 @@ func Stopstream(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Unlock()
 
-	client := search.NewClient(
-		search.SetDebugLogf(log.New(os.Stderr, "", 0).Printf),
-	)
-	queryOut := url.Values{
-		"site":      {site},
-		"video_ids": {videoID},
-	}
-	log.Printf("perform search: %v", queryOut)
-	hits, err := client.Search(context.Background(), queryOut)
-	if err != nil {
-		w.Write(formatResponse(&response, "error", err.Error()))
-		return
-	}
-
-	if hits.TotalHits != 1 {
-		msg := fmt.Sprintf("video with id %s not found", videoID)
-		w.Write(formatResponse(&response, "error", msg))
-		return
-	}
-
 	err = UpdateUserInfo(file.Path(), "stop", videoID)
 	if err != nil {
 		w.Write(formatResponse(&response, "error", err.Error()))
 		return
 	}
 
-	msg := fmt.Sprintf("video with id %s found: %s", videoID, hits.Hits[0].Subset().TitleSv)
+	msg := fmt.Sprintf("video with id %s found: ", videoID)
 	w.Write(formatResponse(&response, "stopped", msg))
 }
